57ecaa96da15bc5eebb146a75d50fd9e318a443e
Lauren Myers <lmmyers2@mail.fhsu.edu>
2023-08-16 10:41:17 -0600

    Got all static files working for website

---
+++ routes/root.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+
+router.get('^/$|/index(.html)?', function(req,res){
+   
+   
+   res.sendFile(path.join(__dirname, '..', 'views', 'index.html') )
+
+});
+
+router.get('/features(.html)?', function(req,res){
+   
+   
+   res.sendFile(path.join(__dirname, '..', 'views', 'features.html') )
+
+});
+
+router.get('/docs(.html)?', function(req,res){
+   
+   
+   res.sendFile(path.join(__dirname, '..', 'views', 'docs.html') )
+
+});
+
+module.exports = router; 
--- server/index.js
+++ server/index.js
@@ -21,24 +21,31 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 //Serve Static Files ...This would be for CSS and Images
-app.use('/', express.static(path.join(__dirname, '/public')));
+app.use('/', express.static(path.join(__dirname, '..', '/public')));
 
 //Routes 
+app.use('/', require('../routes/root'));
 
+// app.all('*', (req, res) => {
 
-app.all('*', (req, res) => {
+//     res.status(404);
+//     if(req.accepts('html')){
+//         res.sendFile(path.join(__dirname, 'views', '404.html'));
+//     }
+//     else if (req.accepts('json')){
+//         res.json({"error": "404 Not Found" });
+//     }
+//     else{
+//         res.type('txt').send("404 Not Found");
+//     }
+// });
 
-    res.status(404);
-    if(req.accepts('html')){
-        res.sendFile(path.join(__dirname, 'views', '404.html'));
-    }
-    else if (req.accepts('json')){
-        res.json({"error": "404 Not Found" });
-    }
-    else{
-        res.type('txt').send("404 Not Found");
-    }
-});
+// ****
+// added for view engine
+// ****
+app.set("views", path.join(__dirname, "../views"));
+app.set("view engine", "ejs");
+
 
 
 
